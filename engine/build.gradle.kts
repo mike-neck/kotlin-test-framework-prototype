4bc9dad534c281ab6e0837a7a6a570e79b70899b
mike_neck <jkrt3333@gmail.com>
2020-03-16 20:12:58 +0900

    ロギング用に slf4j 追加

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.opentest4j:opentest4j:1.2.0")
   implementation("org.junit.platform:junit-platform-engine")
+
+  implementation("org.slf4j:slf4j-api")
 }
 
 tasks.withType<Test> {
